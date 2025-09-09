fa3cd0b2ea25365223b891472e2ddc46c0957d7c
tesssalex-cell <tesssalex@gmail.com>
2025-09-09 19:34:43 +0300

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,1 +1,20 @@
 
+import React from 'react'
+
+export function Card({ className = '', children, ...props }) {
+  return (
+    <div className={`rounded-3xl border border-white/10 bg-black/40 ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ className = '', children, ...props }) {
+  return (
+    <div className={className} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export default Card;
